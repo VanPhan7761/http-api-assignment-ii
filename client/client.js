12713c6859c772c5706ff57e89a8983541dd39ae
Van Phan <80922094+Van-Phan-001@users.noreply.github.com>
2022-02-14 17:35:48 -0500

    added logic to send data to server

--- client/client.js
+++ client/client.js
@@ -1,17 +1,74 @@
-const getUsers = async () =>{
-    console.log("get user");
-}
+const handleResponse = async (response) => {
+      
+    //Grab the content section
+    const content = document.querySelector('#content');
 
-const addUsers = async () =>{
-    console.log("add user");
+    //Based on the status code, display something
+    switch(response.status) {
+      case 200: //success
+        content.innerHTML = `<b>Success</b>`;
+        break;
+      case 201: //created
+        content.innerHTML = '<b>Created</b>';
+        break;
+      case 204: //updated (no response back from server)
+        content.innerHTML = '<b>Updated (No Content)</b>';
+        return;
+      case 400: //bad request
+        content.innerHTML = `<b>Bad Request</b>`;
+        break;
+      default: //any other status code
+        content.innerHTML = `Error code not implemented by client.`;
+        break;
+    }
+
+    let obj = await response.json();
+    
+    //If we have a message, display it.
+    if(obj.message){
+      content.innerHTML += `<p>${obj.message}</p>`;
+    }
+  };
+
+
+
+const sendPost = async (nameForm) => {
+    const nameAction = nameForm.getAttribute('action');
+    const nameMethod = nameForm.getAttribute('method');
+
+    const nameField = nameForm.querySelector('#nameField');
+    const ageField = nameForm.querySelector('#ageField');
+
+    //Build a data string in the FORM-URLENCODED format.
+    const formData = `name=${nameField.value}&age=${ageField.value}`;
+
+    let response = await fetch(nameAction, {
+        method: nameMethod,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Accept': 'application/json',
+        },
+        body: formData,
+      });
+
+
+      //Once we have a response, handle it.
+      handleResponse(response);
 }
 
 const init = () => {
-    const getBtn = document.querySelector("#getBtn");
-    const addBtn = document.querySelector("#addBtn");
+    const nameForm = document.querySelector('#nameForm');
 
-    getBtn.addEventListener('click', getUsers);
-    addBtn.addEventListener('click', addUsers);
+    //replaces default form action with sendPost
+    const addUser = (e) => {
+        e.preventDefault();
+        sendPost(nameForm);
+        return false;
+    }
+
+    nameForm.addEventListener('submit', addUser);
 }
 
+
 window.onload = init;
+
--- src/server.js
+++ src/server.js
@@ -4,32 +4,95 @@ const url = require('url');
 const query = require('querystring');
 
 const htmlHandler = require('./htmlResponses.js');
-
-
+const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-//favicon add later
-const urlStruct = {
-    '/': htmlHandler.getIndex,
-    '/favicon.ico':htmlHandler.getFavicon,
-    '/style.css': htmlHandler.getCSS,
-    '/client.js': htmlHandler.getClientJS,
+//handle POST requests
+const handlePost = (request, response, parsedUrl) => {
+
+    //If they go to /addUser
+    if (parsedUrl.pathname === '/addUser') {
+
+        //Call our below parseBody handler, and in turn pass in the
+        //jsonHandler.addUser function as the handler callback function.
+        parseBody(request, response, jsonHandler.addUser);
+    }
 };
 
-//handles the http request from our server
-const onRequest = (request, response) => {
+const parseBody = (request, response, handler) => {
 
-    const parsedUrl = url.parse(request.url);
-    const params = query.parse(parsedUrl.query);
+    //The request will come in in pieces. We will store those pieces in this
+    //body array.
+    const body = [];
 
-    const acceptedTypes = request.headers.accept.split(',');
+    //if we experience an error w/ requests
+    request.on('error', (err) => {
+        console.dir(err);
+        response.statusCode = 400;
+        response.end();
+    });
+
+    //The second possible event is the "data" event. This gets fired when we
+    //get a piece (or "chunk") of the body. Each time we do, we will put it in
+    //the array. We will always recieve these chunks in the correct order.
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    });
+
+    //when our request ends
+    request.on('end', () => {
+        const bodyString = Buffer.concat(body).toString();
+        const bodyParams = query.parse(bodyString);
+
+        //Once we have the bodyParams object, we will call the handler function. We then
+        //proceed much like we would with a GET request.
+        handler(request, response, bodyParams);
+    });
+};
+
+
+const urlStruct = {
+    '/': htmlHandler.getIndex,
+    '/favicon.ico': htmlHandler.getFavicon,
+    '/style.css': htmlHandler.getCSS,
+    '/client.js': htmlHandler.getClientJS,
+    '/getUsers':jsonHandler.getUsers,
+};
+
+//handle GET requests
+const handleGet = (request, response, parsedUrl) => {
 
     //runs request based off the url struct
     if (urlStruct[parsedUrl.pathname]) {
-        urlStruct[parsedUrl.pathname](request, response, acceptedTypes, params);
+        urlStruct[parsedUrl.pathname](request, response);
+    }
+
+    // if (parsedUrl.pathname === '/style.css') {
+    //     htmlHandler.getCSS(request, response);
+    // } else if (parsedUrl.pathname === '/getUsers') {
+    //     jsonHandler.getUsers(request, response);
+    // } else {
+    //     htmlHandler.getIndex(request, response);
+    // }
+}
+
+//handles the http request from our server
+
+const onRequest = (request, response) => {
+    //parse url into individual parts
+    //returns an object of url parts by name
+    const parsedUrl = url.parse(request.url);
+
+    //const params = query.parse(parsedUrl.query);
+
+    //const acceptedTypes = request.headers.accept.split(',');
+    //check if method was POST, otherwise assume GET 
+    //for the sake of this example
+    if (request.method === 'POST') {
+        handlePost(request, response, parsedUrl);
     } else {
-        urlStruct.notFound(request, response, acceptedTypes, params);
+        handleGet(request, response, parsedUrl);
     }
 };
 
